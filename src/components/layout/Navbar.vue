ca79a61cb5de80821af3d9866d8e9eba07258af1
Xguihur <329179227@qq.com>
2024-04-16 22:59:36 +0800

    feat: 将清空isLogin放在了销毁之前的生命周期中，采取辉哥的建议

--- src/components/layout/Navbar.vue
+++ src/components/layout/Navbar.vue
@@ -2,11 +2,23 @@
   <div class="navbar">
     <div class="left-menu">
       <!-- 汉堡伸缩按钮 -->
-      <hamburger id="hamburger-container" :is-active="isCollapse" class="hamburger-container" @toggleClick="toggleSideBar" />
+      <hamburger
+        id="hamburger-container"
+        :is-active="isCollapse"
+        class="hamburger-container"
+        @toggleClick="toggleSideBar"
+      />
 
       <!-- 面包屑 -->
       <el-breadcrumb separator="/">
-        <el-breadcrumb-item :to="{ path:breadCrumb.path==''?'/':breadCrumb.path}" v-for="breadCrumb in breadCrumbList" :key="breadCrumb.path">{{breadCrumb.meta.title ?breadCrumb.meta.title:'首页' }}</el-breadcrumb-item>
+        <el-breadcrumb-item
+          :to="{ path: breadCrumb.path == '' ? '/' : breadCrumb.path }"
+          v-for="breadCrumb in breadCrumbList"
+          :key="breadCrumb.path"
+          >{{
+            breadCrumb.meta.title ? breadCrumb.meta.title : "首页"
+          }}</el-breadcrumb-item
+        >
       </el-breadcrumb>
     </div>
 
@@ -26,8 +38,7 @@
 </template>
 
 <script>
-import Hamburger from '@/components/Hamburger';
-import request from '@/api/request';
+import Hamburger from "@/components/Hamburger";
 
 export default {
   components: {
@@ -36,48 +47,46 @@ export default {
   props: {
     isCollapse: {
       type: Boolean,
-      default: false
-    }
+      default: false,
+    },
   },
   data() {
     return {
       isActive: false,
-      breadCrumbList: []
+      breadCrumbList: [],
     };
   },
-  computed: {
-  },
+  computed: {},
   watch: {
     // 监听路由的变化,并且是立即执行的
-    '$route': {
+    $route: {
       handler(to, from) {
         // 这个 to 可以拿到跳转后的路由信息，matched 中包含了当前路由的父子信息，可以用来渲染面包屑
         this.breadCrumbList = to.matched;
       },
-      immediate: true
-    }
+      immediate: true,
+    },
   },
   methods: {
     toggleSideBar() {
-      this.$emit('toggleClick');
+      this.$emit("toggleClick");
     },
     handleCommand(command) {
-      if (command === 'home') {
-        this.$router.push('/');
+      if (command === "home") {
+        this.$router.push("/");
       }
-      if (command === 'exit') {
-        localStorage.removeItem('token');
+      if (command === "exit") {
+        localStorage.removeItem("token");
         this.$router.push({
-          path: '/login',
+          path: "/login",
           query: {
-            redirect: this.$route.fullPath
-          }
+            redirect: this.$route.fullPath,
+          },
         });
       }
-    }
+    },
   },
-  mounted() {
-  }
+  mounted() {},
 };
 </script>
 
@@ -87,7 +96,7 @@ export default {
   overflow: hidden;
   position: relative;
   background: #fff;
-  box-shadow: 0 1px 4px rgba(0,21,41,.08);
+  box-shadow: 0 1px 4px rgba(0, 21, 41, 0.08);
   display: flex;
   justify-content: space-between;
   align-items: center;
@@ -102,13 +111,12 @@ export default {
     line-height: 46px;
     height: 100%;
     cursor: pointer;
-    transition: background .3s;
-    -webkit-tap-highlight-color:transparent;
+    transition: background 0.3s;
+    -webkit-tap-highlight-color: transparent;
 
     &:hover {
-      background: rgba(0, 0, 0, .025)
+      background: rgba(0, 0, 0, 0.025);
     }
   }
-
 }
 </style>
--- src/pages/login/index.vue
+++ src/pages/login/index.vue
@@ -1,10 +1,16 @@
 <template>
   <div class="login-container">
-    <el-form ref="loginForm" :model="loginForm" :rules="loginRules" class="login-form" autocomplete="on" label-position="left">
-
+    <el-form
+      ref="loginForm"
+      :model="loginForm"
+      :rules="loginRules"
+      class="login-form"
+      autocomplete="on"
+      label-position="left"
+    >
       <div class="title-container">
         <!-- <h3 class="title">保护动物工作台 </h3> -->
-        <h3 class="title">工作台 </h3>
+        <h3 class="title">工作台</h3>
       </div>
 
       <el-form-item prop="username">
@@ -20,7 +26,12 @@
         />
       </el-form-item>
 
-      <el-tooltip v-model="capsTooltip" content="Caps lock is On" placement="right" manual>
+      <el-tooltip
+        v-model="capsTooltip"
+        content="Caps lock is On"
+        placement="right"
+        manual
+      >
         <el-form-item prop="password">
           <i class="el-icon-lock"></i>
           <el-input
@@ -37,88 +48,97 @@
             @keyup.enter.native="handleLogin"
           />
           <span class="show-pwd" @click="showPwd">
-            <svg-icon :icon-class="passwordType === 'password' ? 'eye' : 'eye-open'" />
+            <svg-icon
+              :icon-class="passwordType === 'password' ? 'eye' : 'eye-open'"
+            />
           </span>
         </el-form-item>
       </el-tooltip>
 
-      <el-button :loading="loading" type="primary" style="width:100%;margin-bottom:30px;" @click.native.prevent="handleLogin">登录</el-button>
+      <el-button
+        :loading="loading"
+        type="primary"
+        style="width: 100%; margin-bottom: 30px"
+        @click.native.prevent="handleLogin"
+        >登录</el-button
+      >
     </el-form>
   </div>
 </template>
 
 <script>
-import { validUsername } from '@/utils/validate';
+import { validUsername } from "@/utils/validate";
 
 export default {
-  name: 'login',
+  name: "login",
   data() {
     const validateUsername = (rule, value, callback) => {
       if (!validUsername(value)) {
-        callback(new Error('请输入正确的用户名'));
+        callback(new Error("请输入正确的用户名"));
       } else {
         callback();
       }
     };
     const validatePassword = (rule, value, callback) => {
       if (!value) {
-        callback(new Error('请输入密码'));
+        callback(new Error("请输入密码"));
       } else {
         callback();
       }
     };
     return {
       loginForm: {
-        username: '',
-        password: ''
+        username: "",
+        password: "",
       },
       loginRules: {
-        username: [{ required: true, trigger: 'blur', validator: validateUsername }],
-        password: [{ required: true, trigger: 'blur', validator: validatePassword }]
+        username: [
+          { required: true, trigger: "blur", validator: validateUsername },
+        ],
+        password: [
+          { required: true, trigger: "blur", validator: validatePassword },
+        ],
       },
-      passwordType: 'password',
+      passwordType: "password",
       capsTooltip: false,
       loading: false,
-      autoLogin: '',
+      autoLogin: "",
     };
   },
   methods: {
     checkCapslock(e) {
       const { key } = e;
-      this.capsTooltip = key && key.length === 1 && (key >= 'A' && key <= 'Z');
+      this.capsTooltip = key && key.length === 1 && key >= "A" && key <= "Z";
     },
     showPwd() {
-      if (this.passwordType === 'password') {
-        this.passwordType = '';
+      if (this.passwordType === "password") {
+        this.passwordType = "";
       } else {
-        this.passwordType = 'password';
+        this.passwordType = "password";
       }
       this.$nextTick(() => {
         this.$refs.password.focus();
       });
     },
     handleLogin() {
-      this.$refs.loginForm.validate(valid => {
+      this.$refs.loginForm.validate((valid) => {
         if (valid) {
           this.loading = true;
           // 调用接口进行登录，登录成功后跳转路由
           // 拿到 token 后存到 localStorage 中
           setTimeout(() => {
             this.$message({
-              message: '登录成功',
-              type: 'success'
+              message: "登录成功",
+              type: "success",
             });
             this.loading = false;
-            localStorage.setItem('token', '123456');
-            // 用于解决接口拦截器重复跳转的缓存
-            if (localStorage.getItem('toLogin')) {
-              localStorage.removeItem('toLogin');
-            }
+            localStorage.setItem("token", "123456");
+
             // 如果路由中有 redirect 参数，就跳转到 redirect 参数指向的页面
             if (this.$route.query.redirect) {
               this.$router.push(this.$route.query.redirect);
             } else {
-              this.$router.push({ path: '/' });
+              this.$router.push({ path: "/" });
             }
           }, 1000);
         } else {
@@ -129,22 +149,27 @@ export default {
     },
   },
   mounted() {
-    if (this.loginForm.username === '') {
+    if (this.loginForm.username === "") {
       this.$refs.username.focus();
-    } else if (this.loginForm.password === '') {
+    } else if (this.loginForm.password === "") {
       this.$refs.password.focus();
     }
   },
+  beforeDestroy() {
+    // 用于解决接口拦截器重复跳转的缓存
+    if (localStorage.getItem("toLogin")) {
+      localStorage.removeItem("toLogin");
+    }
+  },
 };
-
 </script>
 
 <style lang="less">
 /* 修复input 背景不协调 和光标变色 */
 /* Detail see https://github.com/PanJiaChen/vue-element-admin/pull/927 */
 
-@bg:#283443;
-@light_gray:#fff;
+@bg: #283443;
+@light_gray: #fff;
 @cursor: #fff;
 
 @supports (-webkit-mask: none) and (not (cater-color: @cursor)) {
@@ -187,9 +212,9 @@ export default {
 </style>
 
 <style scoped lang="less">
-@bg:#1f7474;
-@dark_gray:#889aa4;
-@light_gray:#eee;
+@bg: #1f7474;
+@dark_gray: #889aa4;
+@light_gray: #eee;
 
 // icon 的样式
 i {
